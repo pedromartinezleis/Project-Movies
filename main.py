ebf45a07088d09378b6533109cfed4f55d03553c
pedromartinezleis <60599980+pedromartinezleis@users.noreply.github.com>
2021-11-09 10:03:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import pandas as pd
 from src.cleaning import *
 
 df = pd.read_csv("data/IMDb movies.csv",encoding = "ISO-8859-1")
-
 # Parámetros API
 token = "k_uzahin65"
 url = "https://imdb-api.com/en/API/SearchMovie/k_uzahin65/Interstellar"
 parameters = {"Authorization": f"token {token}"}
 
 
-nolan = filter_by(df,"director","Cristopher Nolan")
+nolan = filter_by(df,"director","Christopher Nolan")
+print(nolan)
 avengers = filter_by(df,"production_company", "Marvel Studios")
 
 nolan['budget'].apply(cambioBudget)
 avengers['budget'].apply(cambioBudget)
 
-exporto(nolan)
+#exporto(nolan)
+nolan.to_csv("data/nolan.csv", index =False)
 
 
